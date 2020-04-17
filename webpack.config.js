279f86afd3cd83878b813a60a40ad479b26fe4eb
Ryan Duff <ryanduff613@gmail.com>
2020-04-17 12:58:59 -0700

    update webpack config with title of page, confirm npm run build working

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Weekday Checker',
+      title: 'Super Galactic Age Calculator',
       template: './src/index.html',
       inject: 'body'
     })
